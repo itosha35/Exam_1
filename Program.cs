1868bc8348d85dfd5e05330a79b19b1e678bfa4a
unknown <itosha35@gmail.com>
2023-02-13 22:59:52 +0300

    Code has been corrected to match the requrements

--- Program.cs
+++ Program.cs
@@ -1,8 +1,21 @@
-﻿void String3CharArray(string[] mas) // Cutting of extra characters in Strings
+﻿string[] String3CharArray(string[] mas) // Cutting of extra Strings
 {
+    int size = 0;
     for (int i = 0; i < mas.Length; i++)
-        if (mas[i].Length > 3)
-            mas[i] = mas[i].Substring(0, 3);
+        if (mas[i].Length <= 3) size++;
+    if (size == mas.Length) return mas;
+    else if (size != 0)
+    {
+        string[] new_mas = new string[size];
+        for (int i = 0; i < mas.Length; i++)
+            if (mas[i].Length <= 3)
+            {
+                new_mas[new_mas.Length - size] = mas[i];
+                size--;
+            }
+    return new_mas;
+    }
+    else return new[] { "There are NO elements less or equal 3 Characters in Your Array" };
 }
 void ShowArray(string[] mas) // Just Showing the Lineary String Array
 {
@@ -29,10 +42,9 @@ string[] GenStringArray(int number) // Creating the Lineary String Array
     return mas;
 }
 Console.Clear();
-Console.WriteLine("Program Shortcuts the string Array within 3 symbols each element");
+Console.WriteLine("Program shows in the string Array only 3-chars elements");
 Console.Write("Please, set amount of Elements You want to input N = ");
 string[] s_array = GenStringArray(Convert.ToInt32(Console.ReadLine()));
 ShowArray(s_array);
-Console.WriteLine($"Shorted to 3 chars String Array (size is {s_array.Length})");
-String3CharArray(s_array);
-ShowArray(s_array);
+Console.WriteLine($"Shorted Array with up to 3 chars Strings (Array size is {s_array.Length})");
+ShowArray(String3CharArray(s_array));
